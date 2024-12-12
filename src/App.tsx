6184fa3cc15afa221dd3c47f81ba77bfcd7c7372
gerardocrr <gerardo2551.cr@gmail.com>
2024-12-11 23:18:29 -0600

    add CanvasTextOverlay component to App and update container styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import CanvasTextOverlay from "./components/Canvas1";
+
 function App() {
   return (
     <>
-      <div>
+      <div className="container max-w-4xl mx-auto bg-red-600 flex flex-col h-screen justify-center items-center">
         <h1>Happy cards</h1>
+
+        <CanvasTextOverlay />
       </div>
     </>
   );
