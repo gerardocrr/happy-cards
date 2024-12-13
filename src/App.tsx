8c39c7a5b971d34e835acef39c897878dced3fe4
gerardocrr <gerardo2551.cr@gmail.com>
2024-12-12 21:01:28 -0600

    update App component to use 'h-svh' for height styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import CanvasTextOverlay from "./components/Canvas1";
 function App() {
   return (
     <>
-      <div className="container max-w-4xl mx-auto bg-red-600 flex flex-col h-screen justify-center items-center">
+      <div className="container max-w-4xl mx-auto bg-red-600 flex flex-col h-svh justify-center items-center">
         <h1>Happy cards</h1>
 
         <CanvasTextOverlay />
