f134594ecf36ba60840656a76b0e30ae3d1baa8d
gerardocrr <gerardo2551.cr@gmail.com>
2024-12-12 22:13:03 -0600

    add input form for user name and search button in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ function App() {
     <>
       <div className="container max-w-4xl mx-auto bg-red-600 flex flex-col h-svh justify-center items-center">
         <h1>Happy cards</h1>
-
+        <div className="flex flex-col w-1/2">
+          <label htmlFor="">Ingresa tu nombre</label>
+          <input className="mb-5" type="text" />
+          <button className="bg-white rounded-md mb-5">Buscar regalo</button>
+        </div>
         <CanvasTextOverlay />
       </div>
     </>
